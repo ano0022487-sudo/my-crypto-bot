84675d5925cf8d9eed255964063d57a247c0a237
ano0022487-sudo <ano0022487@gmail.com>
2026-08-18 21:47:11 +0800

    Update event-bot-runner.js

--- event-bot-runner.js
+++ event-bot-runner.js
@@ -1,18 +1,11 @@
 'use strict';
 
-const fs = require('fs');
 const path = require('path');
 
-const source = path.join(__dirname, 'event-bot.js');
-const runtime = path.join(__dirname, 'okx-event-bot-runtime.js');
+const runtime = path.join(__dirname, 'event-bot-runtime.js');
 
 try {
-  let code = fs.readFileSync(source, 'utf8');
-  code = code.replace(/tdMode:\s*'cash'/g, "tdMode: 'isolated'");
-
-  fs.writeFileSync(runtime, code, 'utf8');
-  console.log('[Runner] Successfully generated runtime file.');
-
+  console.log('[Runner] Starting Event Contract bot...');
   require(runtime);
 } catch (err) {
   console.error('[Runner Error]', err);
