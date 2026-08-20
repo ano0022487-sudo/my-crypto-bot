1697149c753f61b25ad1972e49879f0d0a2cc21f
ano0022487-sudo <ano0022487@gmail.com>
2026-08-21 00:58:15 +0800

    stabilize canonical Render entrypoint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-/* Single compatibility entrypoint for Render. */
+/* Canonical Render entrypoint. */
 console.log('[DEPLOY ENTRY] MATH-1H-RR-2.1');
 require('./event-bot-runner.js');
