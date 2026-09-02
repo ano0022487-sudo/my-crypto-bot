e9e5dff10f25d914e1e1807cc0ec08ae4396c05c
ano0022487-sudo <ano0022487@gmail.com>
2026-09-03 07:51:17 +0800

    Remove old automated bot entrypoint

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Single Render entrypoint: OKX USDT perpetual contracts only.
-if (process.env.LIVE_TRADING === undefined) process.env.LIVE_TRADING = 'false';
-if (process.env.BOT_STATE_FILE === undefined) process.env.BOT_STATE_FILE = 'perpetual-state.json';
-console.log('[BOOT] OKX Perpetual Contract Bot');
-console.log(`[BOOT] mode=${process.env.LIVE_TRADING === 'true' ? 'LIVE' : 'PAPER'}`);
-require('./telegram-control.js');
-require('./perpetual-bot.js');
